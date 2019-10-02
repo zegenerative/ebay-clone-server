f5076c05051e83367c66d06106d5a88f904f7a6d
zegenerative <zegerdevos@gmail.com>
2019-10-02 11:24:51 +0200

    Add PUT and DELETE endpoints

--- advertisement/router.js
+++ advertisement/router.js
@@ -22,9 +22,38 @@ router.get('/advertisement', (req, res, next) => {
 
 //see specific advertisement(AdDetails)
 router.get('/advertisement/:id', (req, res, next) => {
-    Advertisement.findByPK(req.params.id)
+    Advertisement.findByPk(req.params.id)
     .then(advertisement => {
         res.send(advertisement)
+    })
+    .catch(next)
+})
+
+//update an advertisment(AdDetailsContainer)
+router.put('/advertisement/:id', (req, res, next) => {
+    Advertisement.findByPk(req.params.id)
+    .then(advertisement => {
+        if(!advertisement) {
+            return res.status(404).send('404, Advertisment not found')
+        } else {
+            advertisement.update(req.body)
+            .then(res.send(advertisement))
+        }
+    })
+    .catch(next)
+})
+
+router.delete('/advertisement/:id', (req, res, next) => {
+    Advertisement.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(numDeleted => {
+        if (numDeleted) {
+            return res.status(204).end()
+        }
+        return res.status(404).end()
     })
     .catch(next)
 })
